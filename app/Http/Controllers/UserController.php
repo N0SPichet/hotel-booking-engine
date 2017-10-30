21761692361c6393f85b4039c352fe47ab26438d
N0SPichet <pichetfuengfoo@gmail.com>
2017-10-31 05:52:11 +0700

    แก้ session alert

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -118,7 +118,7 @@ class UserController extends Controller
 
         $user->user_description = $request->input('user_description');
 
-        Session::flash('success', 'Your account has been updated.');
+        Session::flash('success', 'Profile updated successfully.');
 
         $user->save();
 
